990c96f788fa6652057f823d70062fe27e5c171f
Helder Luan <helderluan0202@gmail.com>
2022-07-16 11:32:21 -0300

    Remove .env. Variables are set in the apiURLs file.

--- src/constants/apiURLs.ts
+++ src/constants/apiURLs.ts
@@ -1,4 +1,4 @@
-export const moviesURL = process.env.REACT_APP_API_URL;
-export const searchURL = process.env.REACT_APP_API_SEARCH;
-export const imageURL = process.env.REACT_APP_API_IMAGE;
-export const apiKey = process.env.REACT_APP_API_KEY;
+export const moviesURL = "api_key=c3cd836d2f69cbc7378e649b1f8f7aa1";
+export const searchURL = "https://api.themoviedb.org/3/movie/";
+export const imageURL = "https://api.themoviedb.org/3/search/movie/";
+export const apiKey = "https://image.tmdb.org/t/p/w500";
